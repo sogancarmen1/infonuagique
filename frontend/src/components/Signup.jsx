7e8c367c7d894b6ca87a1da596323609308a414d
sogancarmen1 <sogancarmen1@gmail.com>
2025-07-08 10:53:58 +0100

    tree1231

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -28,7 +28,7 @@ function Signup() {
 		setLoading(true);
 		try {
 			const res = await axios.post(
-				"/users/register",
+				"/api/users/register",
 				{ username, email, password, confirmPassword },
 				{ withCredentials: true }
 			);
