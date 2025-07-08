843a47305b25e1551befb363945d46a53883c0d4
sogancarmen1 <sogancarmen1@gmail.com>
2025-07-08 13:29:12 +0100

    tree123113

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -24,11 +24,12 @@ function Signup() {
 	}, [isLoggedIn, navigate]);
 
 	const handleSignup = async (e) => {
+		const API_URL = import.meta.env.VITE_API_URL;
 		e.preventDefault();
 		setLoading(true);
 		try {
 			const res = await axios.post(
-				"/api/users/register",
+				`${API_URL}/users/register`,
 				{ username, email, password, confirmPassword },
 				{ withCredentials: true }
 			);
